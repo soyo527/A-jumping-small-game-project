0d04fb14cce8c587cfd0ade459a5e489e96fa22a
yzk <yzk@DESKTOP-K6AGF9G>
2024-06-29 17:09:46 +0800

    添加项目文件。

---
+++ test_1/demo.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+    int i;
+    for (i = 0; i < 20; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+ }
---
+++ test_1/demo.cpp
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main()
+{
+	int a;
+
+	scanf("%d", &a);
+	printf("a = %d\n", a);
+
+	return 0;
+}
+
